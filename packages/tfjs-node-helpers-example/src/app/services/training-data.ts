628ce022e184a3c4a37c04a1be524339c1d7d3d7
dusenko@ronasit.com <dusenko@ronasit.com>
2022-10-30 03:38:34 +0600

    feat: load data asynchronously in the example application

---
+++ packages/tfjs-node-helpers-example/src/app/services/training-data.ts
@@ -0,0 +1,71 @@
+import { extractFeatures, Sample, splitSamplesIntoTrainingValidationTestForBinaryClassification } from '@ronas-it/tfjs-node-helpers';
+import { AgeFeatureExtractor } from '../feature-extractors/age';
+import { AnnualSalaryFeatureExtractor } from '../feature-extractors/annual-salary';
+import { GenderFeatureExtractor } from '../feature-extractors/gender';
+import { OwnsTheCarFeatureExtractor } from '../feature-extractors/owns-the-car';
+import dataset from '../../assets/data.json';
+
+export class TrainingDataService {
+  private simulatedDelayMs: number;
+  private trainingSamples: Array<Sample>;
+  private validationSamples: Array<Sample>;
+  private testingSamples: Array<Sample>;
+
+  constructor({ simulatedDelayMs }: { simulatedDelayMs: number }) {
+    this.simulatedDelayMs = simulatedDelayMs;
+  }
+
+  public async initialize(): Promise<void> {
+    const samples = await extractFeatures({
+      data: dataset,
+      inputFeatureExtractors: [
+        new AgeFeatureExtractor(),
+        new AnnualSalaryFeatureExtractor(),
+        new GenderFeatureExtractor()
+      ],
+      outputFeatureExtractor: new OwnsTheCarFeatureExtractor()
+    });
+
+    const { trainingSamples, validationSamples, testingSamples } = splitSamplesIntoTrainingValidationTestForBinaryClassification(samples);
+
+    this.trainingSamples = trainingSamples;
+    this.validationSamples = validationSamples;
+    this.testingSamples = testingSamples;
+  }
+
+  public async getTrainingSamples(skip: number, take: number): Promise<Array<Sample>> {
+    await this.simulateDelay();
+
+    return this.trainingSamples.slice(skip, skip + take);
+  }
+
+  public async getValidationSamples(skip: number, take: number): Promise<Array<Sample>> {
+    await this.simulateDelay();
+
+    return this.validationSamples.slice(skip, skip + take);
+  }
+
+  public async getTestingSamples(skip: number, take: number): Promise<Array<Sample>> {
+    await this.simulateDelay();
+
+    return this.testingSamples.slice(skip, skip + take);
+  }
+
+  public async getValidationSamplesCount(): Promise<number> {
+    await this.simulateDelay();
+
+    return this.validationSamples.length;
+  }
+
+  public async getTestingSamplesCount(): Promise<number> {
+    await this.simulateDelay();
+
+    return this.testingSamples.length;
+  }
+
+  private async simulateDelay(): Promise<void> {
+    if (this.simulatedDelayMs > 0) {
+      await new Promise((resolve) => setTimeout(resolve, this.simulatedDelayMs));
+    }
+  }
+}
